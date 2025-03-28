cf7e99b6cf23d00e294d66087b599d1d7f2dc396
Lia Cardoso <lia.cardoso@ua.pt>
2025-03-28 13:49:33 +0000

    liliana isto é para ti

--- webproj/urls.py
+++ webproj/urls.py
@@ -1,10 +1,12 @@
 # Mapping/routing das URLs para este projeto.
 from django.http import HttpResponse
 from django.urls import path
+from app.Views.stats.stats_view import stats_page
 
 def home(request):
     return HttpResponse("Django está a funcionar!")
 
 urlpatterns = [
     path('', home),
+    path('stats/', stats_page, name='stats'),
 ]
