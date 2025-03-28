2f74855261ef179ae8951731c5ba71e1ec167327
Lia Cardoso <lia.cardoso@ua.pt>
2025-03-28 13:26:08 +0000

    for merge

--- webproj/settings.py
+++ webproj/settings.py
@@ -1,3 +1,27 @@
-# Configurações do projeto Django.
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 ALLOWED_HOSTS = ['*']
 ROOT_URLCONF = 'webproj.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],  # Para encontrar os templates
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
